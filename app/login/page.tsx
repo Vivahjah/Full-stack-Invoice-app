82e47e45fa758a967300fb058436e6dfdbdd5fab
Mengwa Ugo <mengwa44@yahoo.com>
2024-12-04 04:01:06 +0100

    setting up auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signIn } from "../utils/auth";
 
 
 export default function Login() {
@@ -9,11 +10,14 @@ export default function Login() {
         <div className="flex w-full h-screen px-4 justify-center items-center">
             <Card className="max-w-sm">
                 <CardHeader>
-                    <CardTitle  className="text-2xl" >Login</CardTitle>
+                    <CardTitle className="text-2xl" >Login</CardTitle>
                     <CardDescription>Enter your Email to Login to your Account</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <form className="flex flex-col gap-y-2" >
+                    <form action={async () => {
+                        "use server"
+                        await signIn()
+                    }} className="flex flex-col gap-y-2" >
                         <div className="flex flex-col gap-y-2">
                             <Label>Email</Label>
                             <Input placeholder="Hello@hello.com" />
