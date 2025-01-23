db85d0dca37fbf4132c7f16e9f36e3ef45d060e4
Mengwa Ugo <mengwa44@yahoo.com>
2025-01-23 23:29:38 +0100

    added graph

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 import { redirect } from "next/navigation";
 import { auth, signOut } from "../utils/auth";
 import { DashboardBlocks } from "../components/DashboardBlocks";
+import InvoiceGraph from "../components/InvoiceGraph";
 
 
 
@@ -17,6 +18,10 @@ export default async function Dashboard() {
     return (
         <div className="">
             <DashboardBlocks />
+            <div className="grid gap-4 mt-6 lg:grid-cols-3 md:gap-8">
+               <InvoiceGraph />
+                <div className="col-span-1 bg-blue-500">Hello</div>
+            </div>
         </div>
     );
 }
